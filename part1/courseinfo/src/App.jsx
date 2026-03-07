98a8560bb6de8e4b1c52d00eff8fef5fe3a8d2d7
vickmaz <vittoriomazzini@gmail.com>
2026-03-07 20:55:28 +0100

    finished 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,31 +7,48 @@ const Header  = (props) => {
   )
 }
 
+const Content  = () => {
+  const contents = [
+    { name: 'Fundamentals of React', value: 10 },
+    { name: 'Using props to pass data', value: 7 },
+    { name: 'State of a component', value: 14 },
+  ]
+
+  return (
+    <div>
+      <p>{contents[0].name} {contents[0].value}</p>
+      <p>{contents[1].name} {contents[1].value}</p>
+      <p>{contents[2].name} {contents[2].value}</p>
+    </div>
+  )
+}
+
+const Total  = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.totalsum}</p>
+    </div>
+  )
+}
+
 
 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
+  const totalsum = exercises1 + exercises2 + exercises3
 
   return (
     <div>
       <Header course = {course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+      <Content />
+
+      <Total totalsum = {totalsum} />
     </div>
   )
 }
