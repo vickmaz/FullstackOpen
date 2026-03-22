5b17feb0b3ff6517514ad86a1beff2cc9773e4ec
vickmaz <vittoriomazzini@gmail.com>
2026-03-22 09:59:28 +0100

    done exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,11 +4,19 @@ import { useState } from 'react'
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
+  const [goodvalue, setGoodValue] = useState(0)
+
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [badvalue, setBadValue] = useState(0)
+
+  const total = good + neutral + bad 
+  const average = (goodvalue + badvalue) / 2
+  const positivepercent = (good / total) * 100
 
   const HandleGood = () => {
     setGood(good +1)
+    setGoodValue(goodvalue +1)
   }
 
   const HandleNeutral = () => {
@@ -17,6 +25,7 @@ const App = () => {
 
   const HandleBad = () => {
     setBad(bad +1)
+    setBadValue(badvalue -1)
   }
 
 
@@ -33,6 +42,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positivepercent}%</p>
 
     </div>
   )
